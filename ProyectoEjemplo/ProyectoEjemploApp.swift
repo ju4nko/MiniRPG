7c8fe59bdd9fa5449344948761d3982d9e346828
ju4nko <ju4nko@gmail.com>
2026-06-22 09:49:50 +0200

    Initial Commit

---
+++ ProyectoEjemplo/ProyectoEjemploApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProyectoEjemploApp.swift
+//  ProyectoEjemplo
+//
+//  Created by Juanjo on 22/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct ProyectoEjemploApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
